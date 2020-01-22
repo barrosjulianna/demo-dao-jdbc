a0af450dc15710e2399545a2fe2f96e74bc0df9d
Juliana Barros <juliana_barros2@hotmail.com>
2020-01-22 02:07:02 -0300

    Criando interface DepartamentDao e SellerDao

---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,16 @@
+package model.dao;
+
+import java.util.List;
+
+import model_entities.Seller;
+
+
+public interface SellerDao {
+	
+	void insert (Seller obj);
+	void update (Seller obj);
+	void deleteById (Integer id);
+	Seller findById (Integer id); //PEGAR O ID E CONSULTAR NO BD O OBJ ID
+	List<Seller> findAll(); //retornar tudo
+
+}
