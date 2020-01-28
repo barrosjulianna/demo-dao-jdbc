3173da16b5d800af477f0886430647ca4823461b
Juliana Barros <juliana_barros2@hotmail.com>
2020-01-28 14:53:41 -0300

    Implementando findByDepartment

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,20 +1,32 @@
 package application;
 
+import java.util.List;
+import java.util.Scanner;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model_entities.Department;
 import model_entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao= DaoFactory.createSellerDao();
-		
+		System.out.println("Digite o ID desejado");
+		int id= sc.nextInt();
 		System.out.println("===TESTE NUMERO 1 FindById ===");
-		Seller seller= sellerDao.findById(3);
+		Seller seller= sellerDao.findById(id);
 		System.out.println(seller);
 		
+		System.out.println("\n===TESTE NUMERO 2 FindByDepartment ===");
+		Department department = new Department(2,null);
+		List<Seller>list = sellerDao.findByDepartment(department);
 		
+		for(Seller obj: list) {
+			System.out.println(obj);
+		}
 		
 		
 
--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -2,6 +2,7 @@ package model.dao;
 
 import java.util.List;
 
+import model_entities.Department;
 import model_entities.Seller;
 
 
@@ -12,5 +13,6 @@ public interface SellerDao {
 	void deleteById (Integer id);
 	Seller findById (Integer id); //PEGAR O ID E CONSULTAR NO BD O OBJ ID
 	List<Seller> findAll(); //retornar tudo
+	List<Seller> findByDepartment(Department department);
 
 }
